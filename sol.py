5aa973fcf348d519af1217f9a0607c92cbf0160e
Spencer Giddens <giddens2spencer@gmail.com>
2017-02-02 12:35:09 -0700

    Added file for coinsum

--- sol.py
+++ sol.py
@@ -7,4 +7,25 @@ def Coin_Combinations(coins, value):
     returns:
      number of possible combinations to make value with givin coins in "coins"
   """
-  pass
+  # Coin sum problem
+
+  two_pound = 200
+  one_pound = 100
+  fifty_pence = 50
+  twenty_pence = 20
+  ten_pence = 10
+  five_pence = 5
+  two_pence = 2
+  one_pence = 1
+
+  #L = [two_pound, one_pound, fifty_pence, twenty_pence, ten_pence, five_pence, two_pence, one_pence]
+  L = [five_pence, two_pence, one_pence]
+  L.reverse()
+
+  coins = [1] + ([0]*8)
+
+  for money in L:
+      for i in xrange(len(coins)-1):
+          coins[1+i] = 1 + coins[1+i-money]
+
+  return coins
